84a47e93c4388cda44e1b9c7e2ab598ca7727da6
muruw <murumaem@gmail.com>
2019-03-03 13:44:53 +0200

    Hello, fixed your code!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("goodbye world PepeHands");
+        System.out.println("marcus was here :D");
     }
 }
